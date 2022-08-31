a774aaaa5305b1dbdd603f81d3a1168d9de43db3
yhostc <yhostc@gmail.com>
2022-08-31 10:09:34 +0800

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   });
 
   await app.startAllMicroservices();
-  await app.listen(3000);
+  await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
